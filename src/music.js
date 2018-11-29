f5c727c458788849c8614d91192df12655f787bc
Campbell Crowley <github@campbellcrowley.com>
2018-11-28 17:19:30 -0800

    Fix error on music purging

--- src/music.js
+++ src/music.js
@@ -294,6 +294,7 @@ function Music() {
     let entries = Object.entries(broadcasts);
     let now = Date.now();
     for (let i = 0; i < entries.length; i++) {
+      if (!entries[i][1].broadcast) continue;
       let pauseTime = entries[i][1].broadcast.dispatcher.pausedSince;
       if (pauseTime && now - pauseTime > 15 * 60 * 1000) {
         self.debug(entries[i][0] + ' purged: ' + (now - pauseTime));
