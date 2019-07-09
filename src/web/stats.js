92b5db50dd7d9101dbe845ca6afbcc3608846edf
Campbell Crowley <github@campbellcrowley.com>
2019-07-08 20:24:03 -0700

    Add 'shards' as shard count to stat postings

--- src/web/stats.js
+++ src/web/stats.js
@@ -243,7 +243,7 @@ function WebStats() {
           guilds: values.numGuilds,
           guildCount: values.numGuilds,
           users: values.numMembers,
-          // shards: Object.values(values.shardGuilds),
+          shards: values.numShards,
           // shard_id: values.reqShard,
           // shardId: values.reqShard,
           shard_count: values.numShards,
