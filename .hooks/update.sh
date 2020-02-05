50290ac4910c86b0a8f0b9e7bf423d6968cccc72
Campbell Crowley <github@campbellcrowley.com>
2020-02-04 18:00:12 -0800

    Update the update script to work without SSH

---
+++ .hooks/update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if grep -q " git@github.com:" .git/config; then
+  echo "Attempting update from GIT via SSH"
+  ssh-agent bash -c "ssh-add ~/.ssh/sb_id_rsa_nopass && git fetch --all && git reset --hard origin/master && git submodule update && npm i && npm i youtube-dl"; kill $SSH_AGENT_PID
+else
+  echo "Attempting update from GIT via HTTP"
+  git fetch --all && git reset --hard origin/master && git submodule update && npm i && npm i youtube-dl
+fi
