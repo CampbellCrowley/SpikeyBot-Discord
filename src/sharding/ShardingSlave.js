a914e8748e49db01885c9eb6f8e2c38b267f952a
Campbell Crowley <github@campbellcrowley.com>
2020-02-27 19:14:44 -0800

    Fix _settings.config.heartbeat.useMessageStats

--- src/sharding/ShardingSlave.js
+++ src/sharding/ShardingSlave.js
@@ -664,7 +664,7 @@ class ShardingSlave {
 
       this._socket.emit('status', s);
 
-      if (this._config.useMessageStats) {
+      if (this._settings.config.heartbeat.useMessageStats) {
         common.logDebug(
             `Message delta: ${s.messageCountDelta}, Prev: ${prevDelta}`);
         if (prevDelta === 0 && s.messageCountDelta === 0 && !s.isMaster) {
