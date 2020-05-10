f33b5cdad8d0e4068a0d13799caf148c47762a7a
Campbell Crowley <github@campbellcrowley.com>
2020-05-09 21:33:51 -0700

    Add AuditLogActions to moderation.js for reference

--- src/moderation.js
+++ src/moderation.js
@@ -63,6 +63,52 @@ class Moderation extends SubModule {
     ];
     this._saveDir = `${this.common.guildSaveDir}/smited/`;
 
+    /**
+     * @description From {@link external:Discord~AuditLogAction}.
+     * @see https://discord.js.org/#/docs/main/master/typedef/AuditLogAction
+     * @public
+     * @constant
+     * @default
+     */
+    this.AuditLogActions = {
+      ALL: null,
+      GUILD_UPDATE: 1,
+      CHANNEL_CREATE: 10,
+      CHANNEL_UPDATE: 11,
+      CHANNEL_DELETE: 12,
+      CHANNEL_OVERWRITE_CREATE: 13,
+      CHANNEL_OVERWRITE_UPDATE: 14,
+      CHANNEL_OVERWRITE_DELETE: 15,
+      MEMBER_KICK: 20,
+      MEMBER_PRUNE: 21,
+      MEMBER_BAN_ADD: 22,
+      MEMBER_BAN_REMOVE: 23,
+      MEMBER_UPDATE: 24,
+      MEMBER_ROLE_UPDATE: 25,
+      MEMBER_MOVE: 26,
+      MEMBER_DISCONNECT: 27,
+      BOT_ADD: 28,
+      ROLE_CREATE: 30,
+      ROLE_UPDATE: 31,
+      ROLE_DELETE: 32,
+      INVITE_CREATE: 40,
+      INVITE_UPDATE: 41,
+      INVITE_DELETE: 42,
+      WEBHOOK_CREATE: 50,
+      WEBHOOK_UPDATE: 51,
+      WEBHOOK_DELETE: 52,
+      EMOJI_CREATE: 60,
+      EMOJI_UPDATE: 61,
+      EMOJI_DELETE: 62,
+      MESSAGE_DELETE: 72,
+      MESSAGE_BULK_DELETE: 73,
+      MESSAGE_PIN: 74,
+      MESSAGE_UNPIN: 75,
+      INTEGRATION_CREATE: 80,
+      INTEGRATION_UPDATE: 81,
+      INTEGRATION_DELETE: 82,
+    };
+
     this.save = this.save.bind(this);
     this.muteMember = this.muteMember.bind(this);
     this._onMessageDelete = this._onMessageDelete.bind(this);
@@ -232,8 +278,7 @@ class Moderation extends SubModule {
           null);
     } else {
       msg.guild
-          .fetchAuditLogs(
-              {limit: 1, type: this.Discord.AuditLogAction.MESSAGE_DELETE})
+          .fetchAuditLogs({limit: 1, type: this.AuditLogActions.MESSAGE_DELETE})
           .then((logs) => {
             this._finalMessageDeleteSend(
                 msg.guild, msg.author.bot, tag, id, channel, files, msg.content,
@@ -398,7 +443,7 @@ class Moderation extends SubModule {
     } else {
       guild
           .fetchAuditLogs(
-              {limit: 1, type: this.Discord.AuditLogAction.MESSAGE_BULK_DELETE})
+              {limit: 1, type: this.AuditLogActions.MESSAGE_BULK_DELETE})
           .then((logs) => {
             const entry = logs.entries.first();
             const deletedBy = entry && entry.executor && entry.executor.tag;
