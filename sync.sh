3bcb1df598aa7845d1cfeb0d94e7ff7bf76ce68c
Campbell Crowley <github@campbellcrowley.com>
2020-05-13 14:39:20 -0700

    Update sync.sh excludes

--- sync.sh
+++ sync.sh
@@ -5,8 +5,8 @@ for i in $LIST; do
   fi
 done
 
-nice rsync -urpt --rsync-path="nice rsync" --exclude="shards/*" /home/admin/SpikeyBot-Discord/save/ admin@kamino1:/home/admin/SpikeyBot-Discord/save/ &&\
-nice rsync -urpt --rsync-path="nice rsync" --exclude="shards/*" admin@kamino1:/home/admin/SpikeyBot-Discord/save/ /home/admin/SpikeyBot-Discord/save/
+nice rsync -urpt --rsync-path="nice rsync" --exclude-from="sync-exclude.txt" /home/admin/SpikeyBot-Discord/save/ admin@kamino1:/home/admin/SpikeyBot-Discord/save/ &&\
+nice rsync -urpt --rsync-path="nice rsync" --exclude-from="sync-exclude.txt" admin@kamino1:/home/admin/SpikeyBot-Discord/save/ /home/admin/SpikeyBot-Discord/save/
 
 for i in $LIST; do
   if [ -f "$i.DELETEME" ]; then
