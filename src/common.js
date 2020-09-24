4b6f008b0b8a0d97d2ed7ab96f81b008d6a27966
Campbell Crowley <github@campbellcrowley.com>
2020-09-24 11:34:05 -0700

    Fix no mtime on ENOENT

--- src/common.js
+++ src/common.js
@@ -471,7 +471,7 @@ function Common() {
           if (cb) cb(err);
           return;
         }
-        const mtime = stats.mtime.getTime();
+        const mtime = stats && stats.mtime.getTime();
         process.send({_sGetFile: filename, _sGetFileM: mtime}, (err) => {
           if (!err) return;
           process.removeListener('message', listener);
